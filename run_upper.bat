1f0a3b73b24a617ccee7cfc0bdaa633f03b243a6
TDS <tds@tcg3pl.com>
2022-07-22 12:31:18 -0400

    Removing .bat file.

--- run_upper.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-"C:\Users\TechnologyDataServic\anaconda3\python.exe" "C:\Users\TechnologyDataServic\OneDrive - TCG3PL\Documents\Bourbon Trail\QuickBooks\PDFs\upper_right.py"
-
-echo Would you like to open your newly created file?
-pause
-
-start %SystemRoot%\explorer.exe "C:\Users\TechnologyDataServic\OneDrive - TCG3PL\Documents\Bourbon Trail\QuickBooks\PDFs\current_upperright_po3.xlsx"
